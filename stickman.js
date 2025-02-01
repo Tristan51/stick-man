1b7fefc9a1e85add81ef68520b218b82014e25c4
Tristan51 <tristanverhoeve@gmail.com>
2025-02-01 13:37:50 +0100

    Update stickman.js

--- stickman.js
+++ stickman.js
@@ -13,7 +13,7 @@ class Point {
     }
 
     update(dt) {
-        const vx = (this.x - this.oldx) * 0.99; // Apply damping
+        const vx = (this.x - this.oldx) * 0.99; // Damping
         const vy = (this.y - this.oldy) * 0.99;
         this.oldx = this.x;
         this.oldy = this.y;
@@ -108,4 +108,134 @@ class Stickman {
         this.leftHand.update(dt);
         this.rightHand.update(dt);
         this.leftKnee.update(dt);
-       
+        this.rightKnee.update(dt);
+        this.leftFoot.update(dt);
+        this.rightFoot.update(dt);
+
+        // Update sticks
+        for (let i = 0; i < 5; i++) { // Multiple iterations for stability
+            this.sticks.forEach(stick => stick.update());
+        }
+
+        // Constrain points
+        this.head.constrain();
+        this.shoulder.constrain();
+        this.hip.constrain();
+        this.leftElbow.constrain();
+        this.rightElbow.constrain();
+        this.leftHand.constrain();
+        this.rightHand.constrain();
+        this.leftKnee.constrain();
+        this.rightKnee.constrain();
+        this.leftFoot.constrain();
+        this.rightFoot.constrain();
+
+        // Check feet on ground
+        this.feetOnGround = 0;
+        if (this.leftFoot.y >= canvas.height - 50) this.feetOnGround++;
+        if (this.rightFoot.y >= canvas.height - 50) this.feetOnGround++;
+    }
+
+    draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw head
+        ctx.beginPath();
+        ctx.arc(this.head.x, this.head.y, 30, 0, Math.PI * 2);
+        ctx.stroke();
+
+        // Draw body
+        ctx.beginPath();
+        ctx.moveTo(this.shoulder.x, this.shoulder.y);
+        ctx.lineTo(this.hip.x, this.hip.y);
+        ctx.stroke();
+
+        // Draw limbs
+        this.sticks.forEach(stick => {
+            ctx.beginPath();
+            ctx.moveTo(stick.p1.x, stick.p1.y);
+            ctx.lineTo(stick.p2.x, stick.p2.y);
+            ctx.stroke();
+        });
+    }
+
+    getState() {
+        return [
+            this.head.y / canvas.height,
+            (this.head.y - this.head.oldy) / 10,
+            ...Object.values(this.jointAngles).map(a => a / Math.PI)
+        ];
+    }
+}
+
+class AI {
+    constructor() {
+        // Neural network weights
+        this.weights = Array(8 * 10).fill().map(() => Math.random() * 2 - 1);
+        this.reward = 0;
+        this.bestReward = -Infinity;
+        this.bestWeights = [...this.weights];
+    }
+
+    decideAction(state) {
+        const action = new Array(8).fill(0);
+        for (let i = 0; i < this.weights.length; i++) {
+            action[i % 8] += this.weights[i] * state[Math.floor(i / 8)];
+        }
+        return action.map(a => Math.tanh(a)); // Constrain outputs to [-1, 1]
+    }
+
+    mutate() {
+        // Save best weights
+        if (this.reward > this.bestReward) {
+            this.bestReward = this.reward;
+            this.bestWeights = [...this.weights];
+        }
+
+        // Reset to best weights and add noise
+        this.weights = this.bestWeights.map(w => w + (Math.random() - 0.5) * 0.1);
+    }
+}
+
+const stickman = new Stickman();
+const ai = new AI();
+let episode = 0;
+
+function train() {
+    const state = stickman.getState();
+    const action = ai.decideAction(state);
+
+    // Apply actions to joint angles
+    Object.keys(stickman.jointAngles).forEach((joint, i) => {
+        stickman.jointAngles[joint] += action[i] * 0.1;
+    });
+
+    stickman.update(1);
+    stickman.draw();
+
+    // Calculate reward
+    let reward = 0;
+    if (stickman.feetOnGround === 2) {
+        reward = 1;
+        stickman.timeBalanced++;
+        if (stickman.timeBalanced > 180) {
+            console.log("Success!");
+            location.reload();
+        }
+    } else {
+        reward = -0.1;
+        stickman.timeBalanced = 0;
+    }
+    ai.reward += reward;
+
+    // Mutate AI periodically
+    if (episode % 100 === 0) {
+        ai.mutate();
+        ai.reward = 0;
+    }
+
+    episode++;
+    requestAnimationFrame(train);
+}
+
+train();
